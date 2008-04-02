b9a1681f018a73b53e99bd58e707f279a3c668d0
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-04-02 03:23:02 +0000

    allow disabling of install_requires by environment variables
    git-svn-id: https://code.astraw.com/motmot/trunk/wxglvideo@577 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup, find_packages
+import os
+
+kws = {}
+if not int(os.getenv( 'DISABLE_INSTALL_REQUIRES','0' )):
+    kws['install_requires'] = ['numpy>=1.0.4',
+                          'motmot.imops>=0.5.2.dev',
+                          'pyglet>=1.0',
+                          'wxPython>=2.8',
+                          'pygarrayimage>=0.0.2',
+                          ],
 
 setup(name='motmot.wxglvideo',
       description='wx/OpenGL viewer of image sequences',
@@ -11,12 +21,6 @@ This is a subpackage of the motmot family of digital image utilities.
       version='0.6.1',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
-      install_requires = ['numpy>=1.0.4',
-                          'motmot.imops>=0.5.2.dev',
-                          'pyglet>=1.0',
-                          'wxPython>=2.8',
-                          'pygarrayimage>=0.0.2',
-                          ],
       url='http://code.astraw.com/projects/motmot',
       license='BSD',
       zip_safe=True,
@@ -24,5 +28,5 @@ This is a subpackage of the motmot family of digital image utilities.
       namespace_packages = ['motmot'],
       package_data = {'motmot.wxglvideo':['demo.xrc']},
       entry_points = {'gui_scripts': ['wxglvideo_demo=motmot.wxglvideo.demo:main',
-                                      ]}
-      )
+                                      ]},
+      **kws)
