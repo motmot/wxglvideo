c1951549f130b7d46eaa921d7c7ffe1500712a78
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2008-11-17 18:58:10 +0000

    fix install_requires bug
    git-svn-id: https://code.astraw.com/motmot/trunk/wxglvideo@668 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import os
 kws = {}
 if not int(os.getenv( 'DISABLE_INSTALL_REQUIRES','0' )):
     kws['install_requires'] = ['numpy>=1.0.4',
-                          'motmot.imops>=0.5.2.dev',
-                          'pyglet>=1.0',
-                          'wxPython>=2.8',
-                          'pygarrayimage>=0.0.2',
-                          ],
+                               'motmot.imops>=0.5.2.dev',
+                               'pyglet>=1.0',
+                               'wxPython>=2.8',
+                               'pygarrayimage>=0.0.2',
+                               ]
 
 setup(name='motmot.wxglvideo',
       description='wx/OpenGL viewer of image sequences',
