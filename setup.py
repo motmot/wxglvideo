26369370bd30c00e0a75ca57064411a375e8bd00
Andrew Straw <strawman@astraw.com>
2012-05-30 18:30:56 +0200

    bump version for release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ offloading the image operations to OpenGL.
 
 This is a subpackage of the motmot family of digital image utilities.
 """,
-      version='0.6.6',
+      version='0.6.7',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       url='http://code.astraw.com/projects/motmot',
